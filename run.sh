3abb75b35a5ceca116a4461c2c923e7ab9398d6a
Joseph Montanaro <jfmontanaro@modg.org>
2024-05-01 21:32:12 -0400

    adjust timeouts

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ curl -sfL https://get.k3s.io |
     --service-cidr=10.43.0.0/16,2001:cafe:43::/112
 
 echo 'Waiting for cluster to be ready...'
-sleep 10
+sleep 30
 echo 'Creating initial resources'
 kubectl apply -f server.yaml -f service.yaml -f client.yaml
-sleep 3
+sleep 5
 
 echo 'Testing connectivity (should succeed at this point)'
 kubectl exec -it pod/client -- wget -O - http://whoami.default.svc.cluster.local
